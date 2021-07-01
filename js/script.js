da266c39ca28cd6def6145df3741bdb7e5e615c5
Patrick Park <parkpatrickj@gmail.com>
2021-07-01 02:34:22 -0400

    add operation buttons

--- js/script.js
+++ js/script.js
@@ -10,6 +10,15 @@ const createButtons = () => {
         number_button.className = 'number-button';
         number_button.onclick = function() {appendDisplay(number_button)};
     }
+    var operation_array = ['+','-','x','/']
+    for (i = 0; i < 4; i++){
+        const number_button = document.createElement('button');
+        number_button.textContent = operation_array[i];
+        const button_container = document.getElementsByClassName('number-input-keys');
+        button_container[0].appendChild(number_button);
+        number_button.className = 'number-button';
+        number_button.onclick = function() {appendDisplay(number_button)};
+    }
 }
 
 const appendDisplay = (button) => {
